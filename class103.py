6a251766113f2aa9df0c86f12e40ddf786da0c59
aaronsebastian07 <73086898+aaronsebastian07@users.noreply.github.com>
2021-07-08 16:46:44 -0700

    Add files via upload

---
+++ class103.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+import plotly.express as px
+
+df = pd.read_csv("data.csv")
+
+fig = px.line(df, x="Date", y="Cases", color="Country", title='COVID-19 Cases In Countries Around the World')
+
+fig.show()
